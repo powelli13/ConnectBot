6fbf293168bc52ad177379f06326112f8a5bf362
Isaac Powell <powelli91@outlook.com>
2020-03-13 21:21:01 -0700

    added unit tests project with tests for the bit board

---
+++ BitBoardTests/BitBoardTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using ConnectBot;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConnectBotTests
+{
+    [TestClass]
+    public class BitBoardTests
+    {
+        [TestMethod]
+        public void GetNewBoard_ShouldReturnBoardWithAllOpenColumns()
+        {
+            ulong board = BitBoard.GetNewBoard();
+
+            Assert.IsTrue(BitBoard.IsColumnOneOpen(board));
+            Assert.IsTrue(BitBoard.IsColumnTwoOpen(board));
+            Assert.IsTrue(BitBoard.IsColumnThreeOpen(board));
+            Assert.IsTrue(BitBoard.IsColumnFourOpen(board));
+            Assert.IsTrue(BitBoard.IsColumnFiveOpen(board));
+            Assert.IsTrue(BitBoard.IsColumnSixOpen(board));
+            Assert.IsTrue(BitBoard.IsColumnSevenOpen(board));
+        }
+    }
+}
